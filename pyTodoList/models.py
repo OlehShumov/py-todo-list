61c3fc6f4d7bae71a9ba71e83bbbf6b51e426d9b
OlehShumov <shumovoleh@ukr.net>
2023-11-27 12:23:20 +0200

    add models

--- pyTodoList/models.py
+++ pyTodoList/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["-deadline"]
+
+    def __str__(self):
+        return (
+            f"Task: {self.content}, "
+            f"status: {'Done' if self.is_completed else 'Not done'}, "
+        )
